03f566f6958e86110ec22f6ba5900fa9b9d07b25
LucaBazzea <luca.bazzea@gmail.com>
2022-07-14 20:02:45 +0200

    https://stackoverflow.com/questions/60497938/read-exif-metadata-with-go

--- main.go
+++ main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/tiff"
 )
 
-func main() {
-	file, err := os.Stat("test.txt")
+type Printer struct{}
 
+func (p Printer) Walk(name exif.FieldName, tag *tiff.Tag) error {
+	fmt.Printf("%40s: %s\n", name, tag)
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("please give filename as argument")
+	}
+	fname := os.Args[1]
+
+	f, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("File Name:", file.Name())
-	fmt.Println("File Size:", file.Size())
-	fmt.Println("Last Modified:", file.ModTime())
-	fmt.Println("Is Directory:", file.IsDir())
+	x, err := exif.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var p Printer
+	x.Walk(p)
 }
