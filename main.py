08cd49711d08046149444c81c1370e0666d05698
Luca Bazzea <luca.bazzea@gmail.com>
2022-07-26 14:56:20 +0200

    Basic file sorting based on year modified

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+from genericpath import isdir
+from msilib.schema import Directory
 import os
-import datetime
+from datetime import datetime
+import shutil
 
 
-path = r"test.txt"
+for file in os.listdir():
+    unix_time = os.path.getmtime(file) # Get timestamp
+    date_time = datetime.fromtimestamp(unix_time) # Convert unix time to something human readable
 
-m_time = os.path.getmtime(path)
+    date = str(date_time).split()[0] # Get date
+    year = str(date).split("-")[0] # Get year
+    file_ext = str(file).split(".")[-1] # Get file extension
 
-print(f"Modification Time: {m_time}")
+    print(f"{file}: {year}")
+
+    for item in os.listdir(): # Create folder named after the file year
+        if os.path.isdir(item):
+            try:
+                os.mkdir(year)
+            except FileExistsError:
+                pass
+    try:
+        if file_ext == "jpg" or file_ext == "jpeg" or file_ext == "JPG" or file_ext == "png" or file_ext == "gif" or file_ext == "webp" or file_ext == "mkv" or file_ext == "mp4": # I'll find a more elegant solution later - why are there so many names for .jpg
+            shutil.move(file, year)
+    except:
+        print(f"Error - Duplicate name: {file}")
